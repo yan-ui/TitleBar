0c126c396e828b1404bcd82fb4ade51882520938
王焱 <1044658370@qq.com>
2019-11-18 17:11:55 +0800

    Initial commit

---
+++ app/src/main/java/cn/yan/titlebardemo/QuickPreviewActivity.java
@@ -0,0 +1,38 @@
+package cn.yan.titlebardemo;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.wuhenzhizao.titlebar.utils.KeyboardConflictCompat;
+import com.wuhenzhizao.titlebar.widget.CommonTitleBar;
+
+
+/**
+ * Created by liufei on 2017/8/29.
+ */
+
+public class QuickPreviewActivity extends SwipeBackActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_preview);
+        ((CommonTitleBar) findViewById(R.id.titlebar)).setListener(new CommonTitleBar.OnTitleBarListener() {
+            @Override
+            public void onClicked(View v, int action, String extra) {
+                if (action == CommonTitleBar.ACTION_LEFT_TEXT) {
+                    onBackPressed();
+                }
+            }
+        });
+        ((CommonTitleBar) findViewById(R.id.titlebar_3)).showCenterProgress();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        KeyboardConflictCompat.assistWindow(getWindow());
+    }
+}
---
+++ app/src/main/java/cn/yan/titlebardemo/TabFragmentAdapter.java
@@ -0,0 +1,33 @@
+package cn.yan.titlebardemo;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+/**
+ * Created by liufei on 2017/12/1.
+ */
+
+public class TabFragmentAdapter extends FragmentPagerAdapter {
+    private String[] tabs = {"商品", "详情", "评价"};
+
+    public TabFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new TabFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
